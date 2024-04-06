a7a6e31f627608111260ea9ec29c355814a3dfe5
TheWiseSailor <rudasilljay@gmail.com>
2024-04-06 18:02:17 -0400

    header/footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,25 @@
-// Footer.js
-import React from 'react';
-import './Footer.css'
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import './Footer.css';
+
 const Footer = () => {
+  const [isContactClicked, setIsContactClicked] = useState(false);
+  const [isResumeClicked, setIsResumeClicked] = useState(false);
+  const [isPortfolioClicked, setIsPortfolioClicked] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setIsContactClicked(location.pathname === '/properties');
+    setIsResumeClicked(location.pathname === '/news');
+    setIsPortfolioClicked(location.pathname === '/contact'); 
+  }, [location.pathname]);
+
   return (
-    <footer>
-      <p>Footer content goes here</p>
+    <footer className={`footer ${isContactClicked ? 'contact' : ''} ${isResumeClicked ? 'resume' : ''} ${isPortfolioClicked ? 'portfolio' : ''}`}>
+      <p className='ft'>Moore Real Estate &copy; All rights reserved.</p>
     </footer>
   );
-}
+};
 
 export default Footer;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,21 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-
-import logo from "./HeaderImages/logo.png"
-import './Header.css';
+import logo from './HeaderImages/logo.png';
+import './Header.css'; 
 
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isContactClicked, setIsContactClicked] = useState(false);
+  const [isResumeClicked, setIsResumeClicked] = useState(false); 
+  const [isPortfolioClicked, setIsPortfolioClicked] = useState(false); 
+  const location = useLocation();
 
+  useEffect(() => {
+    const { pathname } = location;
+    setIsContactClicked(pathname.toLowerCase() === '/properties');
+    setIsResumeClicked(pathname.toLowerCase() === '/news');
+    setIsPortfolioClicked(pathname.toLowerCase() === '/contact');
+  }, [location]);
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
     <header>
-      <div className={`HeaderPrimary ${isMobileMenuOpen ? 'esports' : ''}`}>
+      <div className={`HeaderPrimary ${isContactClicked ? 'contact' : ''} ${isResumeClicked ? 'resume' : ''} ${isPortfolioClicked ? 'portfolio' : ''}`}>
         <div className="container header">
           <div className="logo-container">
             <Link to="/">
@@ -24,8 +33,9 @@ const Header = () => {
           </div>
           <nav className={`navbar ${isMobileMenuOpen ? 'open' : ''}`}>
             <ul>
-              <li><Link to="/"></Link></li>
-              <li><Link to="/"></Link></li>
+              <li><Link to="/properties" className={isResumeClicked ? 'black-and-white' : ''}>Properties</Link></li>
+              <li><Link to="/news" className={isPortfolioClicked ? 'black-and-white' : ''}>News</Link></li>
+              <li><Link to="/contact" className={isContactClicked ? 'black-and-white' : ''}>Contact</Link></li>
             </ul>
           </nav>
           <div className={`mobile-menu-icon ${isMobileMenuOpen ? 'active' : ''}`} onClick={toggleMobileMenu}>
