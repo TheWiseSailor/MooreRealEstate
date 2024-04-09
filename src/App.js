0bfdd54567614795d3b5f56a28d42b051c77c997
TheWiseSailor <rudasilljay@gmail.com>
2024-04-08 21:15:20 -0400

    Everything works as intended all I need to do is just style it and add more features

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import HomepageRender from "./Components/HomepageRender";
-
+import ContactFormPage from "../src/Components/ContactPageRender/ContactFormPage";
+import AgentProfile from "../src/Components/Agents/AgentProfile";
+import PropertyOfTheDay from "../src/Components/Properties/PropertyOfTheDay";
+import Properties from "../src/Components/Properties/Properties";
+import ViewProperties from "../src/Components/Properties/ViewProperties";
+import NewsPage from "../src/Components/News/News"; 
 const App = () => {
   return (
     <div className="app-container">
@@ -14,7 +19,12 @@ const App = () => {
         <Routes> 
           <Route path="/" element={<HomepageRender />} /> 
           <Route path="/MooreRealEstate" element={<HomepageRender />} /> 
-
+          <Route path="/property/:id" element={<PropertyOfTheDay />} />
+          <Route path="/properties" element={<Properties />} />
+          <Route path="/properties/:id" element={<ViewProperties />} />
+          <Route path="/agent/:name" element={<AgentProfile />} />
+          <Route path="/contact" element={<ContactFormPage />} />
+          <Route path="/news" element={<NewsPage />} />
         </Routes>
         <Footer />
      
